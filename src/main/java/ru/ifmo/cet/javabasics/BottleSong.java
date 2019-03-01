d5da29b300550e53e89e8bb460199dbe0e0645bd
Alexander Butenko <boooff@gmail.com>
2019-03-02 00:53:15 +0300

    initial for Task01

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -31,13 +31,74 @@ package ru.ifmo.cet.javabasics;
  * Нужно ограничить возможность взятия бутылок натуральным число не более 99 бутылок за раз.
  */
 public class BottleSong {
+    private int bottleTakenAtOnce;
+
+    private static final String[] units = {
+            "", "one", "two", "three", "four", "five", "six", "seven",
+            "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
+            "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    };
+
+    private static final String[] tens = {
+            "",        // 0
+            "",        // 1
+            "twenty",  // 2
+            "thirty",  // 3
+            "forty",   // 4
+            "fifty",   // 5
+            "sixty",   // 6
+            "seventy", // 7
+            "eighty",  // 8
+            "ninety"   // 9
+    };
 
     public BottleSong(int bottleTakenAtOnce) {
-        //TODO
+        if (bottleTakenAtOnce <= 0 || bottleTakenAtOnce > 99)
+            throw new IllegalArgumentException ();
+
+        this.bottleTakenAtOnce = bottleTakenAtOnce;
     }
 
     public String getBottleSongLyrics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        StringBuilder sb = new StringBuilder ();
+        int i = 99;
+
+        while (i > 0) {
+            if (i == 1)
+                sb.append (i).append (" bottle of beer on the wall, ").append (i).append (" bottle of beer.\n");
+            else
+                sb.append (i).append (" bottles of beer on the wall, ").append (i).append (" bottles of beer.\n");
+
+            if (i <= bottleTakenAtOnce) {
+                if (i == 1)
+                    sb.append ("Take ").append (number (i)).
+                        append (" down and pass around, no more bottles of beer on the wall.\n");
+                else
+                    sb.append ("Take ").append (number (i)).
+                        append (" down and pass around, no more bottles of beer on the wall.\n");
+
+                i = 0;
+            } else {
+                i -= bottleTakenAtOnce;
+                sb.append ("Take ").append (number (bottleTakenAtOnce)).append (" down and pass around, ").append (i);
+
+                if (i == 1)
+                    sb.append (" bottle of beer on the wall.\n");
+                else
+                    sb.append (" bottles of beer on the wall.\n");
+            }
+        }
+
+        sb.append("No more bottles of beer on the wall, no more bottles of beer.\n").
+           append("Go to the store and buy some more, 99 bottles of beer on the wall.\n");
+
+        return sb.toString ();
+//        throw new UnsupportedOperationException();
+    }
+
+    private static String number(int i) {
+        if (i < 20)
+            return units[i];
+        else return tens[i / 10] + ((i % 10 != 0) ? " " : "") + units[i % 10];
     }
 }
