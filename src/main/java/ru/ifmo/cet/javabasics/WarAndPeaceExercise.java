73d60c89f2fe9579377496eec4758ee5473d9cf6
Artoriys <nikita-alemaskin@rambler.ru>
2019-03-02 16:23:24 +0300

    Task_2 done

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,71 @@
 package ru.ifmo.cet.javabasics;
 
+
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 
 public class WarAndPeaceExercise {
+   private static String result;
+   private static WarAndPeaceExercise warAndPeaceExercise = new WarAndPeaceExercise();
+   private static TreeMap<String, Integer> wordMap = new TreeMap<>();
 
     public static String warAndPeace() {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
+        warAndPeaceExercise.readAndParseWAP(tome12Path.toFile());
+        warAndPeaceExercise.readAndParseWAP(tome34Path.toFile());
+        warAndPeaceExercise.makingResult(entriesSortedByValues(wordMap));
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
-
-        throw new UnsupportedOperationException();
+        return result;
     }
 
+    public void readAndParseWAP(File tome) {
+        try (BufferedReader tomeReader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(tome), "CP1251"))) {
+
+            while (tomeReader.ready()) {
+                String[] words = tomeReader.readLine().toLowerCase().split("[^A-Za-zА-Яа-я]");
+                for (String word: words) {
+                    if (wordMap.containsKey(word)) {
+                        wordMap.put(word, wordMap.get(word) + 1);
+                    } else {
+                        if (word.length() >= 4) wordMap.put(word, 1);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void makingResult(SortedSet<Map.Entry<String,Integer>> wordSet) {
+        Formatter formatter = new Formatter();
+        for (Map.Entry<String, Integer> entry: wordSet){
+            String word = entry.getKey();
+            int amount = entry.getValue();
+
+            if(amount >= 10) {
+                formatter.format("%1$s - %2$d\n", word, amount);
+            }
+        }
+        result = formatter.toString().trim();
+    }
+
+    private static SortedSet<Map.Entry<String,Integer>> entriesSortedByValues(Map<String,Integer> map) {
+        SortedSet<Map.Entry<String,Integer>> sortedEntries = new TreeSet<>(
+                new Comparator<>() {
+                    @Override
+                    public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+                        int res = -e1.getValue().compareTo(e2.getValue());
+                        if (res == 0) res = e1.getKey().compareTo(e2.getKey());
+                        return res;
+                    }
+                }
+        );
+        sortedEntries.addAll(map.entrySet());
+        return sortedEntries;
+    }
 }
