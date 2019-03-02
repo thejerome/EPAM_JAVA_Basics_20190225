ad5db93e475a3969e4a603c6fc4416aa5a35f7e4
MaleFuctor <MaleFuctor@yandex.ru>
2019-03-02 04:22:55 +0300

    first "BottlesSong" task completed

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -1,5 +1,8 @@
 package ru.ifmo.cet.javabasics;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Нужно реализовать констурктор и метод, возвращающий слова песни про бутылки на стене.
  * <p>
@@ -31,13 +34,68 @@ package ru.ifmo.cet.javabasics;
  * Нужно ограничить возможность взятия бутылок натуральным число не более 99 бутылок за раз.
  */
 public class BottleSong {
+    private int bottleTakenAtOnce;
+    private Map<Integer, String> numberToStringMapping = new HashMap<>();
 
-    public BottleSong(int bottleTakenAtOnce) {
-        //TODO
+
+    BottleSong(int bottleTakenAtOnce) {
+        if (bottleTakenAtOnce < 1 || bottleTakenAtOnce > 99) {
+            throw new IllegalArgumentException();
+        }
+        this.bottleTakenAtOnce = bottleTakenAtOnce;
+        initializationOfNumberToStringMapping();
     }
 
-    public String getBottleSongLyrics() {
-        //TODO
-        throw new UnsupportedOperationException();
+    String getBottleSongLyrics() {
+        int remainingBottles = 99;
+        StringBuilder resultSongStringBuilder = new StringBuilder().append("99 bottles of beer on the wall, 99 bottles of beer.\nTake ");
+        String bottle = " bottles ";
+        String bottleTakenAtOnceAsString = convertToString(bottleTakenAtOnce);
+
+        while (remainingBottles > bottleTakenAtOnce) {
+            remainingBottles -= bottleTakenAtOnce;
+            if (remainingBottles == 1) bottle = " bottle ";
+            resultSongStringBuilder
+                    .append(bottleTakenAtOnceAsString)
+                    .append(" down and pass around, ")
+                    .append(remainingBottles)
+                    .append(bottle)
+                    .append("of beer on the wall.\n")
+                    .append(remainingBottles)
+                    .append(bottle)
+                    .append("of beer on the wall, ")
+                    .append(remainingBottles)
+                    .append(bottle)
+                    .append("of beer.\nTake ");
+        }
+        return resultSongStringBuilder
+                .append(convertToString(Math.abs(remainingBottles)))
+                .append(" down and pass around, no more bottles of beer on the wall.\n")
+                .append("No more bottles of beer on the wall, no more bottles of beer.\n")
+                .append("Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+                .toString();
+    }
+
+    private String convertToString(int bottleTakenAtOnce) {
+        return numberToStringMapping.containsKey(bottleTakenAtOnce) ?
+               numberToStringMapping.get(bottleTakenAtOnce) :
+               numberToStringMapping.get(bottleTakenAtOnce / 10 * 10) + " " + numberToStringMapping.get(bottleTakenAtOnce % 10);
+    }
+
+    private void initializationOfNumberToStringMapping() {
+        numberToStringMapping.put(1, "one");
+        numberToStringMapping.put(2, "two");              numberToStringMapping.put(3, "three");
+        numberToStringMapping.put(4, "four");             numberToStringMapping.put(5, "five");
+        numberToStringMapping.put(6, "six");              numberToStringMapping.put(7, "seven");
+        numberToStringMapping.put(8, "eight");            numberToStringMapping.put(9, "nine");
+        numberToStringMapping.put(10, "ten");             numberToStringMapping.put(11, "eleven");
+        numberToStringMapping.put(12, "twelve");          numberToStringMapping.put(13, "thirteen");
+        numberToStringMapping.put(14, "fourteen");        numberToStringMapping.put(15, "fifteen");
+        numberToStringMapping.put(16, "sixteen");         numberToStringMapping.put(17, "seventeen");
+        numberToStringMapping.put(18, "eighteen");        numberToStringMapping.put(19, "nineteen");
+        numberToStringMapping.put(20, "twenty");          numberToStringMapping.put(30, "thirty");
+        numberToStringMapping.put(40, "forty");           numberToStringMapping.put(50, "fifty");
+        numberToStringMapping.put(60, "sixty");           numberToStringMapping.put(70, "seventy");
+        numberToStringMapping.put(80, "eighty");          numberToStringMapping.put(90, "ninety");
     }
 }
