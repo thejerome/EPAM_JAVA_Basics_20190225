0f8753566968b1baa0d09fbae30fa9528a3f8cab
MaleFuctor <MaleFuctor@yandex.ru>
2019-03-02 09:55:50 +0300

    second ("WarAndPeaceExercise") with loops task completed

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,80 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 
-public class WarAndPeaceExercise {
+class WarAndPeaceExercise {
+    private static TreeMap<String, Integer> dictionary = new TreeMap<>();
+    private static Set<Words> wordsSet = new TreeSet<>();
 
-    public static String warAndPeace() {
+    static String warAndPeace() {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
-
-        throw new UnsupportedOperationException();
+        readAndPutInDictionary(tome12Path.toFile());
+        readAndPutInDictionary(tome34Path.toFile());
+        return writeToFile();
     }
 
+    private static void readAndPutInDictionary(File file) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            while (bufferedReader.ready()) {
+                String[] strings = bufferedReader.readLine().split("[^а-яА-Яa-zA-Z]");
+                for (String word : strings) {
+                    String string = word.toLowerCase();
+                    if (string.length() >= 4) {
+                        if (dictionary.containsKey(string)) {
+                            dictionary.put(string, dictionary.get(string) + 1);
+                        } else {
+                            dictionary.put(string, 1);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String writeToFile() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, Integer> stringIntegerEntry : dictionary.entrySet()) {
+            Integer integer = stringIntegerEntry.getValue();
+            if (integer > 9) {
+                wordsSet.add(new Words(stringIntegerEntry.getKey(), integer));
+            }
+        }
+        for (Words words : wordsSet) {
+            stringBuilder.append(words.toString());
+        }
+        return stringBuilder.toString().trim();
+    }
+
+    private static class Words implements Comparable {
+        String word;
+        Integer amount;
+
+        public Words(String word, Integer amount) {
+            this.word = word;
+            this.amount = amount;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Words words = (Words) o;
+            int result = words.amount - this.amount;
+            if (result == 0) {
+                result = this.word.compareTo(words.word);
+            }
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return word + " - " + amount + "\n";
+        }
+    }
 }
