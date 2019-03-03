33ea5fb24ab51f5607bfcc01e4ff70ddff4ab44d
BerezinD <ghost_6600@mail.ru>
2019-03-03 17:58:45 +0300

    Task 2 added solution

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,85 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 
 public class WarAndPeaceExercise {
+
+    private Map<String, Integer> result = new HashMap<>();
 
     public static String warAndPeace() {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
-
-        throw new UnsupportedOperationException();
+        WarAndPeaceExercise exercise = new WarAndPeaceExercise();
+        exercise.readTheFile(tome12Path);
+        exercise.readTheFile(tome34Path);
+        return exercise.getResult();
     }
 
+    private String getResult() {
+        StringBuilder result = new StringBuilder();
+        this.sortByValue(this.result);
+        for (Map.Entry<String, Integer> entry : this.result.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            if (value < 10) continue;
+            result.append(key)
+                    .append(" - ")
+                    .append(value)
+                    .append("\n");
+        }
+        result.deleteCharAt(result.lastIndexOf("\n"));
+        return result.toString();
+    }
+
+    private void sortByValue(Map<String, Integer> mapToSort) {
+        // Create a list from elements of HashMap
+        List<Map.Entry<String, Integer>> list =
+                new LinkedList<>(mapToSort.entrySet());
+
+        // Sort the list
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                if (o1.getValue().compareTo(o2.getValue()) == 0)
+                    return o1.getKey().compareTo(o2.getKey());
+                return -(o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // put data from sorted list to hashmap
+        HashMap<String, Integer> temp = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        this.result = temp;
+    }
+
+    private void readTheFile(Path fileToRead) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(
+                        new File(String.valueOf(fileToRead))), Charset.forName("windows-1251"))
+        )) {
+            String temp;
+            while ((temp = reader.readLine()) != null) {
+                for (String word :
+                        temp.split("[^a-zA-Zа-яА-Я]")) {
+                    if (word != null && word.length() >= 4) {
+                        word = word.toLowerCase();
+                        int n = this.result.get(word) == null ? 0 : this.result.get(word);
+                        if (n >= 0) {
+                            this.result.put(word, ++n);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
