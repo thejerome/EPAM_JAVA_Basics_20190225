4597677d17d045ef15305495b1fa3aad475d0b49
Myazin Nikita <kart1058@list.ru>
2019-03-04 16:06:50 +0300

    t01

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -1,5 +1,7 @@
 package ru.ifmo.cet.javabasics;
 
+import java.util.Formatter;
+
 /**
  * Нужно реализовать констурктор и метод, возвращающий слова песни про бутылки на стене.
  * <p>
@@ -32,12 +34,64 @@ package ru.ifmo.cet.javabasics;
  */
 public class BottleSong {
 
+    private static String[] SPECIAL_UNITS =
+            {"", "one", "two", "three", "four",
+                    "five", "six", "seven", "eight", "nine",
+                    "ten", "eleven", "twelve", "thirteen", "fourteen"};
+    private static String[] TENS = {"", "", "twen", "thir", "for", "fif", "six", "seven", "eigh", "nine"};
+
+    private int intBottleTakenAtOnce;
+
     public BottleSong(int bottleTakenAtOnce) {
-        //TODO
+        if (bottleTakenAtOnce < 1 || bottleTakenAtOnce > 99) {
+            throw new IllegalArgumentException("Amount of bottles should be natural number less than 100");
+        }
+
+        intBottleTakenAtOnce = bottleTakenAtOnce;
     }
 
     public String getBottleSongLyrics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        Formatter formatter = new Formatter();
+        String stringBottleTakenAtOnce = intToString(intBottleTakenAtOnce);
+        String bottles = "bottles";
+
+        int bottlesAmount = 99;
+
+        while (bottlesAmount > intBottleTakenAtOnce) {
+            formatter.format("%1$d %2$s of beer on the wall, %1$d %2$s of beer.\n", bottlesAmount, bottles)
+                    .format("Take %s down and pass around, ", stringBottleTakenAtOnce);
+
+            bottlesAmount -= intBottleTakenAtOnce;
+            bottles = bottlesAmount == 1 ? "bottle" : bottles;
+
+            formatter.format("%1$d %2$s of beer on the wall.\n", bottlesAmount, bottles);
+        }
+
+        stringBottleTakenAtOnce = intToString(bottlesAmount);
+
+        formatter.format("%1$d %2$s of beer on the wall, %1$d %2$s of beer.\n", bottlesAmount, bottles)
+                .format("Take %1$s down and pass around, no more bottles of beer on the wall.\n", stringBottleTakenAtOnce);
+
+        formatter.format("No more bottles of beer on the wall, no more bottles of beer.\n" +
+                "Go to the store and buy some more, 99 bottles of beer on the wall.\n");
+
+        return formatter.toString();
+    }
+
+    private String intToString(int bottleTakenAtOnce) {
+
+        if (bottleTakenAtOnce < SPECIAL_UNITS.length) {
+            return SPECIAL_UNITS[bottleTakenAtOnce];
+        }
+
+        int tens = bottleTakenAtOnce / 10;
+        int units = bottleTakenAtOnce % 10;
+
+        if (tens != 1) {
+            String separator = units == 0 ? "" : " ";
+            return TENS[tens] + "ty" + separator + SPECIAL_UNITS[units];
+        } else {
+            return TENS[units] + "teen";
+        }
     }
 }
