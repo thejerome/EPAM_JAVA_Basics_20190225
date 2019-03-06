cceec14cd8ddf23d0519e5b6678731cfa4e618d1
josdafsos <wano90210@yandex.ru>
2019-03-07 01:36:24 +0300

    Task_1 bottle_song completed

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -32,12 +32,86 @@ package ru.ifmo.cet.javabasics;
  */
 public class BottleSong {
 
+    private int bottleTakenAtOnce;
+    private int bottleCount = 99;
     public BottleSong(int bottleTakenAtOnce) {
-        //TODO
+        if(bottleTakenAtOnce < 1 || bottleTakenAtOnce > 99){
+            throw new IllegalArgumentException();
+        }
+        this.bottleTakenAtOnce = bottleTakenAtOnce;
     }
 
     public String getBottleSongLyrics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        StringBuffer sb = new StringBuffer();
+        int erase = 0;
+        while (bottleCount > 0) {
+            sb.append(bottleCount + bottlesOnWall(bottleCount)+ ", "+
+                    bottleCount + bottlesOfBeer(bottleCount) + ".\n");
+            erase = bottleCount > bottleTakenAtOnce ? bottleTakenAtOnce : bottleCount;
+            bottleCount -= erase;
+            if(bottleCount>0) {
+                sb.append("Take " + numToString(erase) + "down and pass around, " +
+                        bottleCount + bottlesOnWall(bottleCount)+".\n");
+            }else{
+                sb.append("Take " + numToString(erase) + "down and pass around, " +
+                        "no more" + bottlesOnWall(bottleCount)+".\n");
+            }
+        }
+        sb.append("No more bottles of beer on the wall, no more bottles of beer.\n" +
+                  "Go to the store and buy some more, 99 bottles of beer on the wall.\n");
+
+        return sb.toString();
+    }
+    private String numToString(int i){
+        String tens="";
+        String ones="";
+        if( i > 9 && i < 20){
+            switch(i){
+                case 10: return "ten ";
+                case 11: return "eleven ";
+                case 12: return "twelve ";
+                case 13: return "thirteen ";
+                case 14: return "fourteen ";
+                case 15: return "fifteen ";
+                case 16: return "sixteen ";
+                case 17: return "seventeen ";
+                case 18: return "eighteen ";
+                case 19: return "nineteen ";
+            }
+        }
+        switch(i/10){
+            case 2: tens = "twenty "; break;
+            case 3: tens = "thirty "; break;
+            case 4: tens = "forty "; break;
+            case 5: tens = "fifty "; break;
+            case 6: tens = "sixty "; break;
+            case 7: tens = "seventy "; break;
+            case 8: tens = "eighty "; break;
+            case 9: tens = "ninety "; break;
+        }
+        switch(i%10){
+            case 1: ones ="one ";break;
+            case 2: ones ="two ";break;
+            case 3: ones = "three ";break;
+            case 4: ones = "four "; break;
+            case 5: ones = "five ";break;
+            case 6: ones = "six ";break;
+            case 7: ones = "seven "; break;
+            case 8: ones = "eight ";break;
+            case 9: ones = "nine ";break;
+        }
+        return tens + ones;
+    }
+    private String bottlesOnWall(int i){
+        if(i!=1){
+            return " bottles of beer on the wall";
+        }
+        return " bottle of beer on the wall";
+    }
+    private String bottlesOfBeer(int i){
+        if(i!=1){
+            return " bottles of beer";
+        }
+        return " bottle of beer";
     }
 }
