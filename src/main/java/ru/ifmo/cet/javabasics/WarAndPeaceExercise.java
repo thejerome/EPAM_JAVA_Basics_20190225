57f60351d85f7a30c5e24478ca7a5da150ca9d2d
iaramer <44377558+iaramer@users.noreply.github.com>
2019-03-06 18:07:48 +0300

    Added "countTheWordsToMap" method

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -3,19 +3,19 @@ package ru.ifmo.cet.javabasics;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WarAndPeaceExercise {
 
     private static String result;
+    private Map<String, Integer> map = new HashMap<String, Integer>();
 
     public static String warAndPeace() {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+
 
         return result;
     }
@@ -26,6 +26,20 @@ public class WarAndPeaceExercise {
         InputStream inputStream2 = new FileInputStream(tome12Path.toFile());
         InputStream inputStream = new SequenceInputStream(inputStream1, inputStream2);
         return new BufferedReader(new InputStreamReader(inputStream, "windows-1251"));
+    }
+
+    private void countTheWordsToMap(BufferedReader br) throws IOException {
+
+        while(br.ready()) {
+            String[] s = br.readLine().toLowerCase().split("[^\\p{L}]");
+            for (String word : s) {
+                if (map.containsKey(word)) {
+                    map.put(word, map.get(word) + 1);
+                } else {
+                    map.put(word, 1);
+                }
+            }
+        }
     }
 
 
