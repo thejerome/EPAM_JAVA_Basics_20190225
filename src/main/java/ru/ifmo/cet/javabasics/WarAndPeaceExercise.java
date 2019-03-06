953e11624b093a9a75186c43e01c740af329f305
iaramer <44377558+iaramer@users.noreply.github.com>
2019-03-06 17:48:16 +0300

    Added "concatinatedTomes" method

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -6,6 +6,8 @@ import java.io.*;
 
 public class WarAndPeaceExercise {
 
+    private static String result;
+
     public static String warAndPeace() {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
@@ -15,7 +17,17 @@ public class WarAndPeaceExercise {
         // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
         // TODO Also omit any word with lengths less than 4 and frequency less than 10
 
-        throw new UnsupportedOperationException();
+        return result;
     }
+
+
+    private BufferedReader concatinatedTomes(Path tome12Path, Path tome34Path) throws IOException {
+        InputStream inputStream1 = new FileInputStream(tome12Path.toFile());
+        InputStream inputStream2 = new FileInputStream(tome12Path.toFile());
+        InputStream inputStream = new SequenceInputStream(inputStream1, inputStream2);
+        return new BufferedReader(new InputStreamReader(inputStream, "windows-1251"));
+    }
+
+
 
 }
