2ebba694718c65a5534d7ea6536640e02141dcbd
robopyh <andreytretnikov@mail.ru>
2019-03-08 23:43:51 +0300

    Complete task 2.

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,50 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 
 public class WarAndPeaceExercise {
+    private static final int MIN_WORD_LENGTH = 4;
+    private static final int MIN_FREQUENCY = 10;
 
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        List<String> lines = Files.readAllLines(tome12Path, Charset.forName("windows-1251"));
+        lines.addAll(Files.readAllLines(tome34Path, Charset.forName("windows-1251")));
 
-        throw new UnsupportedOperationException();
+        Map<String, Integer> wordFrequency = new HashMap<>();
+        Integer count;
+
+        for (String line : lines) {
+            String[] words = line.split("[^a-zA-Zа-яА-Я]");
+            for (String word : words) {
+                if (word.length() >= MIN_WORD_LENGTH) {
+                    word = word.toLowerCase();
+                    count = wordFrequency.get(word);
+                    wordFrequency.put(word, count == null ? 1 : count + 1);
+                }
+            }
+        }
+
+        List<Map.Entry<String, Integer>> sortedList = new ArrayList<>(wordFrequency.entrySet());
+        sortedList.sort(Map.Entry.comparingByKey());
+        sortedList.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        Iterator<Map.Entry<String, Integer>> iterator = sortedList.iterator();
+        Map.Entry<String, Integer> entry;
+
+        while ((entry = iterator.next()).getValue() >= MIN_FREQUENCY) {
+            stringBuilder.append(entry.getKey()).append(" - ").append(entry.getValue()).append("\n");
+        }
+
+        return stringBuilder.toString().trim();
     }
-
 }
