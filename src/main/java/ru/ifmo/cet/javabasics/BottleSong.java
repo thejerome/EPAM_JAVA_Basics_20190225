9df4a67a82afe9c927493a1519d51eee148cbb3e
akharlov <dronindigo@gmail.com>
2019-03-09 00:07:11 +0300

    Bottle song. Initial commit.

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -1,5 +1,8 @@
 package ru.ifmo.cet.javabasics;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * Нужно реализовать констурктор и метод, возвращающий слова песни про бутылки на стене.
  * <p>
@@ -31,13 +34,80 @@ package ru.ifmo.cet.javabasics;
  * Нужно ограничить возможность взятия бутылок натуральным число не более 99 бутылок за раз.
  */
 public class BottleSong {
+    private int bottleTakenAtOnce;
 
     public BottleSong(int bottleTakenAtOnce) {
-        //TODO
+        this.bottleTakenAtOnce = bottleTakenAtOnce;
     }
 
     public String getBottleSongLyrics() {
-        //TODO
-        throw new UnsupportedOperationException();
+
+        if (bottleTakenAtOnce <= 0 || bottleTakenAtOnce > 99){
+            throw new IllegalArgumentException();
+        }
+
+        int bottlesLeft = 99;
+        String bottleWord, bottlesLeftWord;
+        StringBuilder song = new StringBuilder();
+        Locale locale = new Locale("en", "US");
+        ResourceBundle bundle = ResourceBundle.getBundle("ru.ifmo.cet.javabasics.Numbers", locale);
+
+        while (bottlesLeft >= bottleTakenAtOnce){
+
+            if (bottlesLeft == 1){
+                bottleWord = "bottle";
+            } else {
+                bottleWord = "bottles";
+            }
+
+            String firstSentence = bottlesLeft + " " + bottleWord + " of beer on the wall, "
+                    + bottlesLeft + " " + bottleWord + " of beer.\n";
+
+            bottlesLeft = bottlesLeft - bottleTakenAtOnce;
+
+            if (bottlesLeft == 1){
+                bottleWord = "bottle";
+            } else {
+                bottleWord = "bottles";
+            }
+
+            if (bottlesLeft == 0){
+                bottlesLeftWord = "no more";
+            } else {
+                bottlesLeftWord = String.valueOf(bottlesLeft);
+            }
+
+            String secondSentence = "Take " + bundle.getString(String.valueOf(bottleTakenAtOnce)) +
+                    " down and pass around, " + bottlesLeftWord + " " + bottleWord + " of beer on the wall.\n";
+
+            song.append(firstSentence);
+            song.append(secondSentence);
+        }
+
+        if (bottlesLeft != 0){
+
+            if (bottlesLeft == 1){
+                bottleWord = "bottle";
+            } else {
+                bottleWord = "bottles";
+            }
+
+            String firstSentence = bottlesLeft + " " + bottleWord + " of beer on the wall, "
+                    + bottlesLeft + " " + bottleWord + " of beer.\n";
+
+            bottleWord = "bottles";
+            bottlesLeftWord = "no more";
+
+            String secondSentence = "Take " + bundle.getString(String.valueOf(bottlesLeft)) +
+                    " down and pass around, " + bottlesLeftWord + " " + bottleWord + " of beer on the wall.\n";
+
+            song.append(firstSentence);
+            song.append(secondSentence);
+        }
+
+        song.append("No more bottles of beer on the wall, no more bottles of beer.\n");
+        song.append("Go to the store and buy some more, 99 bottles of beer on the wall.\n");
+
+        return song.toString();
     }
 }
