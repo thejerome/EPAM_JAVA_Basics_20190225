40e11548d92b06672dda719f275d832b27bb8bbc
NewKid <newkidlp2@gmail.com>
2019-03-16 03:58:41 +0300

    WarAndPeace Konovalov Dmitry

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,5 +1,7 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -9,12 +11,12 @@ public class WarAndPeaceExercise {
     public static String warAndPeace() {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
-
+        File WAP1 = new File(tome12Path.toString());
+        Wap1.
         // TODO map lowercased words to its amount in text and concatenate its entries.
         // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
         // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
         // TODO Also omit any word with lengths less than 4 and frequency less than 10
-
         throw new UnsupportedOperationException();
     }
 
